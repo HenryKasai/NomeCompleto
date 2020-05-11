072aea26003f4f315f0469183f782a7cff1a23fe
HenryKasai <henrykasai1775@gmail.com>
2020-05-11 11:44:13 -0300

    Exercício resolvido.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace NomeCompleto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write("Olá usuário, seu nome é: ");
+            string nome = Console.ReadLine();
+            Console.Write("E o seu sobrenome é: ");
+            string sobrenome = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Nome completo: " + nome + " " + sobrenome + ".");
+            Console.WriteLine("Nome de catálogo: " + sobrenome + ", " + nome + ".");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.ReadKey();
+        }
+    }
+}
